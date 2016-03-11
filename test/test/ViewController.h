cd0481f8e0e68ff74ce7cc97b459bb820e2dfcd2
ZhengXiangteng <249891176@qq.com>
2016-03-11 16:42:36 +0800

    1. 测试

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by ZhengXiangteng on 16/3/11.
+//  Copyright © 2016年 前辈丶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by ZhengXiangteng on 16/3/11.
+//  Copyright © 2016年 前辈丶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
